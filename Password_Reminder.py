c7d96a762a8d1042cfe0afa6fc0d737aca32a658
zehraturan <zehraturangr@gmail.com>
2021-04-24 01:06:47 +0300

    Password_Remonder.py file added

---
+++ Password_Reminder.py
@@ -0,0 +1,7 @@
+name = "Zehra"
+password = "W@12"
+user_name = input("Please enter your name: ").strip().title()
+if user_name == name :
+    print(f"Hello, {user_name} The password is : {password}")
+else :
+    print(f"Hello, {user_name}! See you later")
