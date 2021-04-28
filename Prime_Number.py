91b28c0f5ab000235c96bb3c639d3bc9d5c2a2f0
zehraturan <zehraturangr@gmail.com>
2021-04-28 07:03:35 +0300

    Prime_number.py was added

---
+++ Prime_Number.py
@@ -0,0 +1,19 @@
+number = int(input("Enter the number: "))
+if number > 0 :
+    if number == 2 :
+        print (f"{number} is a prime number")
+    elif number % 2 :
+        i = 2
+        control = True
+        while i < number :
+            if (number / i).is_integer() :
+                print (f"{number} is not a prime number")
+                control = False
+                break
+            i+=1
+        if control :
+            print (f"{number} is a prime number")    
+    else :
+        print(f"{number} is not a prime number.")
+else :
+    print(f"{number} is not a prime number.")
